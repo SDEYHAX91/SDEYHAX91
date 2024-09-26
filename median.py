4667f343793c991792b0a03a4dbed78cc4ea02f6
SDEYHAX91 <sourjya.dey2004@gmail.com>
2024-09-26 20:58:32 +0530

    Update median.py

--- median.py
+++ median.py
@@ -1,10 +1,19 @@
 def median(lst):
-    t = len(lst)
-    if t % 2 != 0:
-        return lst[(t-1)//2]
+    c = len(lst)
+    if c % 2 == 0:
+        med = (lst[c//2] + lst[(c//2)-1])/2
+    elif c % 2 != 0:
+        med = lst[c//2]
     else:
-        med = (lst[(t-1)//2] + lst[t//2])/2
-        return med
+        return None
+    if med == int(med):
+        return int(med)
+    return med
     
-lst = [1,2,3,4,5,6,8,9]
-print("Median = ",median(lst))
+    
+n = int(input("Enter size: "))
+lst = []
+for i in range(n):
+    el = float(input(f"{i+1}. Enter value =  "))
+    lst.append(el)
+print("Median = ",median(lst))    
